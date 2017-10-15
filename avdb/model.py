4abd65ddc17a70a3c8be300ad89e32d74b9a9bbb
Michael Meffie <mmeffie@sinenomine.net>
2017-10-14 22:08:30 -0400

    add model list methods

--- avdb/model.py
+++ avdb/model.py
@@ -41,9 +41,11 @@ class Cell(Base):
     name = Column(String(255), unique=True)
     active = Column(Integer, default=1)
     added = Column(DateTime, default=func.now())
+
     def __repr__(self):
         return "<Cell(id=%d, name='%s', active=%d, added='%s')>" % \
                (self.id, self.name, self.active, self.added)
+
     @staticmethod
     def add(session, name=name):
         cell = session.query(Cell).filter_by(name=name).first()
@@ -51,6 +53,10 @@ class Cell(Base):
             cell = Cell(name=name)
             session.add(cell)
         return cell
+
+    @staticmethod
+    def list(session):
+        return session.query(Cell).filter_by(active=1).all()
 
 class Node(Base):
     __tablename__ = 'node'
@@ -63,16 +69,24 @@ class Node(Base):
     added = Column(DateTime, default=func.now())
     checked = Column(DateTime)
     replied = Column(DateTime)
+
     def __repr__(self):
         return "<Node(id=%d, cell_id=%d, name='%s', address='%s', active=%d, added='%s', checked='%s', replied='%s')>" \
                % (self.id, self.cell_id, self.name, self.address, self.active, self.added, self.checked, self.replied)
+
     @staticmethod
-    def add(session, cell, name='', address=None):
+    def add(session, cell, name='', address=None, **kwargs):
         node = session.query(Node).filter_by(address=address).first()
         if node is None:
-            node = Node(cell=cell, address=address, name=name)
+            node = Node(cell=cell, address=address, name=name, **kwargs)
             session.add(node)
         return node
+
+    @staticmethod
+    def list(session):
+        return session.query(Node,Cell) \
+                .filter(Node.cell_id == Cell.id) \
+                .all()
 
 class Service(Base):
     __tablename__ = 'service'
@@ -87,16 +101,26 @@ class Service(Base):
     checked = Column(DateTime)
     replied = Column(DateTime)
     changed = Column(DateTime)
+
     def __repr__(self):
         return "<Service(id=%d, node_id=%d, name='%s', version='%s', active=%d, checked='%s', replied='%s', changed='%s')>" \
                 % (self.id, self.node_id, self.name, self.version, self.active, self.checked, self.replied, self.changed)
+
     @staticmethod
-    def add(session, node, name=''):
+    def add(session, node, name='', **kwargs):
         service = session.query(Service).filter_by(node=node, name=name).first()
         if service is None:
-            service = Service(node=node, name=name)
+            service = Service(node=node, name=name, **kwargs)
             session.add(service)
         return service
+
+    @staticmethod
+    def list(session):
+        return session.query(Service, Node, Cell) \
+                .filter(Service.id == 1) \
+                .filter(Service.node_id == Node.id) \
+                .filter(Node.cell_id == Cell.id) \
+                .all()
 
 def example():
     from pprint import pprint
@@ -106,11 +130,18 @@ def example():
     node = Node.add(session, cell, name='afsdb1', address='1.2.3.4')
     Service.add(session, node, name='ptserver')
     Service.add(session, node, name='vlserver')
+    Service.add(session, node, name='foobar', active=0)
     session.commit()
     print "cells"; pprint(session.query(Cell).all())
     print "nodes"; pprint(session.query(Node).all())
     print "services"; pprint(session.query(Service).all())
+    print "active cells"; print Cell.list(session)
+    print "active nodes"; print Node.list(session)
+    print "active services"
+    for s in Service.list(session):
+        print s
 
 if __name__ == "__main__":
     init_db()
     example()
+
