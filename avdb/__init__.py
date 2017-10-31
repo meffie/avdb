99ee1c0e1d1d3687953b91f6c9a11b5906a8ae80
Michael Meffie <mmeffie@sinenomine.net>
2017-10-31 16:21:45 -0400

    export subcommand functions to the avdb module
    This makes it easier to call the commands as regular functions
    in a python program.

--- avdb/__init__.py
+++ avdb/__init__.py
@@ -1,2 +1,15 @@
+
+"""afs version tracking database"""
+
 from avdb.__version__ import VERSION as __version__
 __version__ # to hush lint
+
+from avdb.__main__ import help
+from avdb.__main__ import version
+from avdb.__main__ import init
+from avdb.__main__ import import__
+from avdb.__main__ import activate
+from avdb.__main__ import deactivate
+from avdb.__main__ import list__
+from avdb.__main__ import scan
+from avdb.__main__ import report
