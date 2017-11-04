3f99b23727078f712abd03c7ad22387e328a8455
Michael Meffie <mmeffie@sinenomine.net>
2017-11-04 11:14:14 -0400

    do dns lookups in avdb scan
    Since the dns lookups can take time, and we want to redo them before
    every version scan in case the records have been changed, do the dns
    look ups as part of the scan, instead of just once when the cell names
    are added.  Do the dns lookups in parallel using multiple processes,
    since the timeouts can take a while.
    
    Reinstate the avdb add subcommand to take just a cellname an optional
    description, and revert avdb import to be just for importing cellnames
    and hosts from CellServDB files.

--- avdb/__init__.py
+++ avdb/__init__.py
@@ -5,6 +5,7 @@ from avdb.__version__ import VERSION as __version__
 from avdb.__main__ import help_
 from avdb.__main__ import version_
 from avdb.__main__ import init_
+from avdb.__main__ import add_
 from avdb.__main__ import import_
 from avdb.__main__ import activate_
 from avdb.__main__ import deactivate_
@@ -17,6 +18,7 @@ __version__
 help_
 version_
 init_
+add_
 import_
 activate_
 deactivate_
